8841eb8e6f5e411f2bc168a75bfdb5dd753ac98b
ther3tyle <shbae0121@gmail.com>
2020-08-08 02:19:57 +0900

    .

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -6,13 +6,14 @@ import Home from "../components/Home/Home";
 import Blog from "../components/Blog/Blog";
 
 const Router = (props) => {
+    const baseURL = process.env.PUBLIC_URL
     return (
         <div>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <BrowserRouter basename={baseURL}>
                 <Layout>
-                    <Route path={process.env.PUBLIC_URL + "/"} exact component={Home}/>
-                    <Route path={process.env.PUBLIC_URL + "/blog"} exact component={Blog}/>
-                    <Route path={process.env.PUBLIC_URL + "/blog/post"} exact component={Blog}/>
+                    <Route path={baseURL + "/"} exact component={Home}/>
+                    <Route path={baseURL + "/blog"} exact component={Blog}/>
+                    <Route path={baseURL + "/blog/post"} exact component={Blog}/>
                 </Layout>
             </BrowserRouter>
         </div>
